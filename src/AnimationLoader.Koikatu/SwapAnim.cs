de3c90714c5edf7383f8bd89abef393509d405ea
Keelhauled <keelhauledhs@gmail.com>
2021-03-28 01:04:18 +0200

    Cleanup

--- src/AnimationLoader.Koikatu/SwapAnim.cs
+++ src/AnimationLoader.Koikatu/SwapAnim.cs
@@ -1,3 +1,4 @@
+using System;
 using BepInEx;
 using HarmonyLib;
 using IllusionUtility.GetUtility;
@@ -11,23 +12,28 @@ using UnityEngine;
 using UnityEngine.UI;
 using static HFlag;
 
+[assembly: System.Reflection.AssemblyFileVersion(AnimationLoader.Koikatu.SwapAnim.Version)]
+
 namespace AnimationLoader.Koikatu
 {
-    [BepInPlugin(nameof(SwapAnim), nameof(SwapAnim), "1.0")]
+    [BepInPlugin(GUID, "SwapAnim", Version)]
     public class SwapAnim : BaseUnityPlugin
     {
-        //void Awake()
-        //{
-        //    var nai = Singleton<HSceneProc>.Instance.flags.nowAnimationInfo;
-        //    Console.WriteLine($"{nai.id}, {nai.mode}, {nai.nameAnimation}, {nai.posture}, {nai.numCtrl}, {nai.kindHoushi}, {nai}");
-        //    var lstAnimInfo = Traverse.Create(Singleton<HSceneProc>.Instance).Field<List<HSceneProc.AnimationListInfo>[]>("lstAnimInfo").Value;
-        //    for (var i = 0; i < 10; i++)
-        //    {
-        //        File.WriteAllLines($"lst{i}.txt", lstAnimInfo[i].Select(x => $"{x.id}, {x.mode}, {x.nameAnimation}, {x.posture}, {x.numCtrl}, {x.kindHoushi}, {x}").ToArray());
-        //    }
-        //}
+        public const string GUID = "SwapAnim";
+        public const string Version = "1.0.0";
+    
+        // void Awake()
+        // {
+        //     var nai = Singleton<HSceneProc>.Instance.flags.nowAnimationInfo;
+        //     Console.WriteLine($"{nai.id}, {nai.mode}, {nai.nameAnimation}, {nai.posture}, {nai.numCtrl}, {nai.kindHoushi}, {nai}");
+        //     var lstAnimInfo = Traverse.Create(Singleton<HSceneProc>.Instance).Field<List<HSceneProc.AnimationListInfo>[]>("lstAnimInfo").Value;
+        //     for (var i = 0; i < 10; i++)
+        //     {
+        //         File.WriteAllLines($"lst{i}.txt", lstAnimInfo[i].Select(x => $"{x.id}, {x.mode}, {x.nameAnimation}, {x.posture}, {x.numCtrl}, {x.kindHoushi}, {x}").ToArray());
+        //     }
+        // }
 
-        internal class SwapAnimationInfo
+        public class SwapAnimationInfo
         {
             public string PathFemale;
             public string PathMale;
@@ -35,19 +41,19 @@ namespace AnimationLoader.Koikatu
             public string AnimationName;
 
             public EMode Mode;
-            public KindHoushi kindHoushi; //0, 1, 2 -> Hand/Mouth/Breasts
-            public PositionCategory[] categories; //0, 1, 2 -> Hand/Mouth/Breasts
+            public KindHoushi kindHoushi;
+            public PositionCategory[] categories;
             public int DonorPoseId;
         }
 
-        internal enum KindHoushi
+        public enum KindHoushi
         {
             Hand = 0,
             Mouth = 1,
             Breasts = 2
         }
 
-        internal enum PositionCategory
+        public enum PositionCategory
         {
             LieDown = 0,
             Stand = 1,
@@ -63,8 +69,8 @@ namespace AnimationLoader.Koikatu
             SquadDesk = 11,
         }
 
-        Harmony hi = Harmony.CreateAndPatchAll(typeof(SwapAnim), nameof(SwapAnim));
-        void OnDestroy() => hi.UnpatchAll(nameof(SwapAnim));
+        private readonly Harmony hi = Harmony.CreateAndPatchAll(typeof(SwapAnim), nameof(SwapAnim));
+        private void OnDestroy() => hi.UnpatchAll(nameof(SwapAnim));
 
         private static Dictionary<EMode, List<SwapAnimationInfo>> AnimationDict = new Dictionary<EMode, List<SwapAnimationInfo>>();
         private static SwapAnimationInfo swapAnimationInfo;
@@ -73,44 +79,42 @@ namespace AnimationLoader.Koikatu
         private static PositionCategory category;
 
         [HarmonyPostfix, HarmonyPatch(typeof(HSceneProc), "ChangeAnimator")]
-        static void post_HSceneProc_ChangeAnimator(HSceneProc.AnimationListInfo _nextAinmInfo)
+        private static void post_HSceneProc_ChangeAnimator(HSceneProc.AnimationListInfo _nextAinmInfo)
         {
             SwapAnimation();
         }
 
         [HarmonyPrefix, HarmonyPatch(typeof(HSceneProc), "ChangeCategory")]
-        static void pre_HSceneProc_ChangeCategory(int _category)
+        private static void pre_HSceneProc_ChangeCategory(int _category)
         {
             category = (PositionCategory)_category;
         }
 
         //todo: not this
-        static HSceneProc.AnimationListInfo Clone_AnimationListInfo(HSceneProc.AnimationListInfo orig)
+        private static HSceneProc.AnimationListInfo Clone_AnimationListInfo(HSceneProc.AnimationListInfo orig)
         {
             return JsonUtility.FromJson<HSceneProc.AnimationListInfo>(JsonUtility.ToJson(orig));
         }
 
         [HarmonyTranspiler, HarmonyPatch(typeof(HSprite), "OnChangePlaySelect")]
-        static IEnumerable<CodeInstruction> tpl_HSprite_OnChangePlaySelect(IEnumerable<CodeInstruction> instructions)
+        private static IEnumerable<CodeInstruction> tpl_HSprite_OnChangePlaySelect(IEnumerable<CodeInstruction> instructions)
         {
             //force position change even if position appears to match. Prevents clicks from being eaten.
             return new CodeMatcher(instructions)
-                .MatchForward(false, new[]
-                {
+                .MatchForward(false, 
                     new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(HSprite), "flags")),
                     new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(HFlag), "nowAnimationInfo")),
                     new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(HSceneProc.AnimationListInfo), "id"))
-                })
-                .MatchForward(false, new[]
-                {
+                )
+                .MatchForward(false,
                     new CodeMatch(OpCodes.Ret)
-                })
+                )
                 .SetAndAdvance(OpCodes.Nop, null)
                 .InstructionEnumeration();
         }
 
         [HarmonyPostfix, HarmonyPatch(typeof(HSprite), "LoadMotionList")]
-        static void post_HSprite_LoadMotionList(HSprite __instance, List<HSceneProc.AnimationListInfo> _lstAnimInfo, GameObject _objParent)
+        private static void post_HSprite_LoadMotionList(HSprite __instance, List<HSceneProc.AnimationListInfo> _lstAnimInfo, GameObject _objParent)
         {
             if(_lstAnimInfo == null || _lstAnimInfo.Count == 0) return;
             var first = _lstAnimInfo[0];
@@ -127,19 +131,16 @@ namespace AnimationLoader.Koikatu
             glg.constraintCount = 15;
             glg.childAlignment = TextAnchor.UpperRight;
 
-            if(!AnimationDict.TryGetValue(first.mode, out List<SwapAnimationInfo> swapAnimations)) return;
+            if(!AnimationDict.TryGetValue(first.mode, out var swapAnimations)) return;
 
-            //Console.WriteLine(category);
-            //Console.WriteLine(swapAnimations.Count);
             foreach(var anim in swapAnimations.Where(x => (int)x.kindHoushi == first.kindHoushi && (!x.categories.Any() || x.categories.Contains(category))))
             {
                 var tg = _objParent.GetComponent<ToggleGroup>();
-                var btn = Instantiate(__instance.objMotionListNode);
+                var btn = Instantiate(__instance.objMotionListNode, _objParent.transform, false);
                 var aic = btn.AddComponent<HSprite.AnimationInfoComponent>();
 
                 aic.info = Clone_AnimationListInfo(lstAnimInfo[(int)first.mode].First(x => x.id == anim.DonorPoseId));
 
-                btn.transform.SetParent(_objParent.transform, false);
                 var label = btn.transform.Find("TextMeshPro Text").GetComponent<TextMeshProUGUI>();
 
                 var image = btn.transform.FindLoop("Background").gameObject.GetComponent<Image>();
@@ -160,7 +161,7 @@ namespace AnimationLoader.Koikatu
 
                 btn.GetComponent<PointerAction>().listClickAction.Add(() =>
                 {
-                    var nai = __instance.flags.nowAnimationInfo;
+                    //var nai = __instance.flags.nowAnimationInfo;
                     //force position change even if position appears to match. Prevents clicks from being eaten.
                     //if (nai.mode != aic.info.mode || nai.id != aic.info.id)
                     {
@@ -200,8 +201,8 @@ namespace AnimationLoader.Koikatu
             mi.Clear();
 
             //TODO: MotionIKData.
-            mi.Add(new MotionIK(female, null));
-            mi.Add(new MotionIK(male, null));
+            mi.Add(new MotionIK(female));
+            mi.Add(new MotionIK(male));
             mi.ForEach(mik =>
             {
                 mik.SetPartners(mi);
@@ -212,20 +213,22 @@ namespace AnimationLoader.Koikatu
         }
 
         [HarmonyPostfix, HarmonyPatch(typeof(HSceneProc), "CreateAllAnimationList")]
-        static void post_HSceneProc_CreateAllAnimationList(HSceneProc __instance)
+        private static void post_HSceneProc_CreateAllAnimationList(HSceneProc __instance)
         {
             Init();
         }
 
-        void Update()
+#if DEBUG
+        private void Update()
         {
             if(Input.GetKeyDown(KeyCode.RightControl))
             {
                 Init();
             }
         }
+#endif
 
-        static void Init()
+        private static void Init()
         {
             // "master" list, can we still use this term
             lstAnimInfo = Traverse.Create(Singleton<HSceneProc>.Instance).Field<List<HSceneProc.AnimationListInfo>[]>("lstAnimInfo").Value;
@@ -236,8 +239,8 @@ namespace AnimationLoader.Koikatu
             foreach(var f in new DirectoryInfo("anim_imports").GetFiles("*.json"))
             {
                 var sai = JsonUtility.FromJson<SwapAnimationInfo>(File.ReadAllText(f.FullName));
-                if(!AnimationDict.TryGetValue(sai.Mode, out List<SwapAnimationInfo> list))
-                    AnimationDict[sai.Mode] = (list = new List<SwapAnimationInfo>());
+                if(!AnimationDict.TryGetValue(sai.Mode, out var list))
+                    AnimationDict[sai.Mode] = list = new List<SwapAnimationInfo>();
                 list.Add(sai);
             }
         }
