adeb2506c2bdf67001a5726978fb678eafbe901c
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2021-03-30 23:29:07 +0300

    Bumb version

--- src/AnimationLoader.Koikatu/SwapAnim.cs
+++ src/AnimationLoader.Koikatu/SwapAnim.cs
@@ -23,7 +23,7 @@ namespace AnimationLoader.Koikatu
     public class SwapAnim : BaseUnityPlugin
     {
         public const string GUID = "SwapAnim";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         private const string ManifestRootElement = "AnimationLoader";
         private const string ManifestArrayItem = "Animation";
