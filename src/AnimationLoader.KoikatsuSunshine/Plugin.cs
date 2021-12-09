b65c8d6f38d5cf2b0c76b1d129d9c2070332576e
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2021-12-09 17:33:42 -0500

    BaseUnityPlugin definition game specific KK has more BepInProcess definitions

---
+++ src/AnimationLoader.KoikatsuSunshine/Plugin.cs
@@ -0,0 +1,23 @@
+﻿//
+// Entry for KKS
+//
+using BepInEx;
+using KKAPI;
+
+
+namespace AnimationLoader
+{
+    //
+    // TODO:
+    // Only load for main game no Studio
+    // Not loading any type of animations in Studio the loading is just taking time at the moment
+    //
+    [BepInPlugin(GUID, DisplayName, Version)]
+    [BepInDependency(Sideloader.Sideloader.GUID, Sideloader.Sideloader.Version)]
+    [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    [BepInProcess(KoikatuAPI.GameProcessName)]
+    [BepInProcess(KoikatuAPI.StudioProcessName)]
+    public partial class SwapAnim : BaseUnityPlugin
+    {
+    }
+}
