93def88eae3b4ed080f1e0a49d9ecbc8b30ccf30
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2021-12-11 04:11:06 -0500

    Conditial compilation for Studio

--- src/AnimationLoader.KoikatsuSunshine/Plugin.cs
+++ src/AnimationLoader.KoikatsuSunshine/Plugin.cs
@@ -11,12 +11,14 @@ namespace AnimationLoader
     // TODO:
     // Only load for main game no Studio
     // Not loading any type of animations in Studio the loading is just taking time at the moment
-    //
-    [BepInPlugin(GUID, DisplayName, Version)]
+    // [BepInProcess(KoikatuAPI.StudioProcessName)]
+    [BepInPlugin(PInfo.GUID, PInfo.PluginName, PInfo.Version)]
     [BepInDependency(Sideloader.Sideloader.GUID, Sideloader.Sideloader.Version)]
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
     [BepInProcess(KoikatuAPI.GameProcessName)]
+#if DEBUG
     [BepInProcess(KoikatuAPI.StudioProcessName)]
+#endif
     public partial class SwapAnim : BaseUnityPlugin
     {
     }
