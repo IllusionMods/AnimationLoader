f5a4322b9e1e8bf443837c8b9ca157ee84bda4d2
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2021-12-11 04:13:54 -0500

    Use new PInfo constants

--- src/AnimationLoader.Koikatu/Plugin.cs
+++ src/AnimationLoader.Koikatu/Plugin.cs
@@ -3,13 +3,14 @@
 //
 using BepInEx;
 
+
 namespace AnimationLoader
 {
     /// <summary>
     /// TODO:
     /// At this stage KK working code will be modified the least possible.
     /// </summary>
-    [BepInPlugin(GUID, "Animation Loader", Version)]
+    [BepInPlugin(PInfo.GUID, "Animation Loader", PInfo.Version)]
     [BepInDependency(Sideloader.Sideloader.GUID, Sideloader.Sideloader.Version)]
     public partial class SwapAnim : BaseUnityPlugin
     {
