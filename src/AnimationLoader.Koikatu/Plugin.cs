2a9cb2c8ac0c4959a0ff8ab5070aa4719ae7eec4
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2021-12-11 23:36:23 -0500

    Cleanup

--- src/AnimationLoader.Koikatu/Plugin.cs
+++ src/AnimationLoader.Koikatu/Plugin.cs
@@ -1,30 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿//
+// Entry point for AnimationLoader.Koikatu
+//
 using BepInEx;
-using HarmonyLib;
-using IllusionUtility.GetUtility;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using BepInEx.Configuration;
-using BepInEx.Logging;
-using Illusion.Extensions;
-using Sideloader.AutoResolver;
-using Studio;
-using TMPro;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using static HFlag;
-using Manager;
 using KKAPI;
 
 namespace AnimationLoader
 {
-    
+
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
     [BepInDependency(Sideloader.Sideloader.GUID, Sideloader.Sideloader.Version)]
     [BepInPlugin(PInfo.GUID, PInfo.PluginDisplayName, PInfo.Version)]
