69858387150376899d33e7b752b6635b553b7e70
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2021-12-11 23:32:34 -0500

    Cleanup

--- src/AnimationLoader.Koikatu/Hooks.LoadMotionList.cs
+++ src/AnimationLoader.Koikatu/Hooks.LoadMotionList.cs
@@ -1,12 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
+﻿using System;
+using System.Collections;
+using BepInEx;
 using HarmonyLib;
 using IllusionUtility.GetUtility;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using Illusion.Extensions;
+using Sideloader.AutoResolver;
+using Studio;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static HFlag;
+using Manager;
 
 
 namespace AnimationLoader
