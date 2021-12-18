c15e31111bbeaadf3cd4ad815c34cd6ae5491f04
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2021-12-18 18:06:45 -0500

    Added ability to move characters at start of animation.

---
+++ src/AnimationLoader.Core/Fixes/Move.AnimationInfo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using HarmonyLib;
+using Illusion.Extensions;
+
+using KKAPI.Utilities;
+
+using static HFlag;
+using static Illusion.Component.UI.MouseButtonCheck;
+
+namespace AnimationLoader
+{
+    public partial class SwapAnim
+    {
+        internal class AnimationInfo
+        {
+            internal string _Guid;
+            internal EMode _mode;
+            internal int _id;
+            internal SwapAnimationInfo _anim;
+            public string Guid
+            {
+                get { return _Guid; }
+            }
+            public EMode Mode
+            {
+                get { return _mode; }
+            }
+            public int Id
+            {
+                get { return _id; }
+            }
+
+            public string Key
+            {
+                get { return $"{_Guid}-{_mode}-{_id:D3}"; }
+            }
+
+            public SwapAnimationInfo SwapAnim
+            {
+                get { return _anim; }
+            }
+
+            public AnimationInfo(HSceneProc.AnimationListInfo animation)
+            {
+                _mode = animation.mode;
+                swapAnimationMapping.TryGetValue(animation, out var anim);
+                if (anim != null)
+                {
+                    // AnimationLoader animation
+                    _Guid = anim.Guid;
+                    _id = anim.StudioId;
+                    _anim = anim;
+                }
+                else
+                {
+                    // Game animation
+                    _anim = null;
+                    _Guid = PInfo.GUID + ".move";
+                    _id = animation.id;
+                }
+            }
+
+            // TODO: add support for SwapAnimationInfo
+            public static string GetKey(HSceneProc.AnimationListInfo animation)
+            {
+                string Guid;
+                EMode mode;
+                int id;
+
+                mode = animation.mode;
+                swapAnimationMapping.TryGetValue(animation, out var anim);
+                if (anim != null)
+                {
+                    Guid = anim.Guid;
+                    id = anim.StudioId;
+                }
+                else
+                {
+                    Guid = PInfo.GUID + ".move";
+                    id = animation.id;
+                }
+                return $"{Guid}-{mode}-{id:D3}";
+            }
+        }
+    }
+}
---
+++ src/AnimationLoader.Core/Fixes/Move.Characters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using UnityEngine;
+
+using HarmonyLib;
+using Illusion.Extensions;
+
+using KKAPI.Utilities;
+
+using static HFlag;
+using static Illusion.Component.UI.MouseButtonCheck;
+
+namespace AnimationLoader
+{
+    public partial class SwapAnim
+    {
+        internal class MoveCharacter
+        {
+            public static void Move(ChaControl character, Vector3 move)
+            {
+                try
+                {
+                    var xAxis = character.transform.right * move.x;
+                    var yAxis = new Vector3(0, move.y, 0);
+                    var zAxis = character.transform.forward * move.z;
+
+                    character.transform.position += xAxis;
+                    character.transform.position += yAxis;
+                    character.transform.position += zAxis;
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError($"0015: Cannot adjust {character.name}.");
+                }
+            }
+        }
+    }
+}
