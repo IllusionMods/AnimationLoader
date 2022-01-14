92134dcdf69f0abf60834235019581066fa90b4e
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-01-14 11:17:27 -0500

    VS generated files

---
+++ src/AnimationLoader.KoikatsuSunshine/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.MoveController.SetOriginalPosition")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.AnimationInfo.IsAnimationLoader(AnimationLoader.SwapAnimationInfo)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "<Pending>", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.GetMoveController(ChaControl)~AnimationLoader.SwapAnim.MoveController")]
