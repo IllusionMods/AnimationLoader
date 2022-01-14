2d5991e433de2868f7f254b62befcf71ca8baac9
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-01-14 11:10:49 -0500

    Add controller as part of key

--- src/AnimationLoader.Core/Fixes/Move.AnimationInfo.cs
+++ src/AnimationLoader.Core/Fixes/Move.AnimationInfo.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-
-using HarmonyLib;
-using Illusion.Extensions;
-
-using KKAPI.Utilities;
+﻿
+using KKAPI;
 
 using static HFlag;
-using static Illusion.Component.UI.MouseButtonCheck;
+
 
 namespace AnimationLoader
 {
     public partial class SwapAnim
     {
+        /// <summary>
+        /// Class to help uniquely identified the animations
+        /// </summary>
         public class AnimationInfo
         {
+            internal SwapAnimationInfo _anim;
+
             internal string _Guid;
             internal EMode _mode;
             internal int _id;
-            internal SwapAnimationInfo _anim;
+            internal string _controller;
+
             public string Guid {
                 get { return _Guid; }
             }
@@ -32,13 +31,14 @@ namespace AnimationLoader
             }
 
             public string Key {
-                get { return $"{_Guid}-{_mode}-{_id:D3}"; }
+                get { return $"{_Guid}-{_mode}-{_controller}-{_id:D3}"; }
             }
 
             public AnimationInfo()
             {
                 _Guid = string.Empty;
                 _mode = EMode.none;
+                _controller = string.Empty;
                 _id = -1;
             }
 
@@ -55,13 +55,17 @@ namespace AnimationLoader
                     // AnimationLoader animation
                     _Guid = anim.Guid;
                     _id = anim.StudioId;
+                    // Some manifest don't define a StudioId using this to make sure
+                    // key is one-to-one (so far so god)
+                    _controller = anim.ControllerFemale;
                     _anim = anim;
                 }
                 else
                 {
                     // Game animation
                     _anim = null;
-                    _Guid = PInfo.GUID + ".move";
+                    _Guid = KoikatuAPI.GameProcessName;
+                    _controller = animation.paramFemale.path.file;
                     _id = animation.id;
                 }
             }
@@ -70,7 +74,7 @@ namespace AnimationLoader
                 AnimationInfoHelper(animation);
             }
 
-            public static string GetKey(HSceneProc.AnimationListInfo animation)
+            public static string GetKey(HSceneProc.AnimationListInfo animation, bool withguid = true)
             {
                 string Guid;
                 EMode mode;
@@ -85,20 +89,35 @@ namespace AnimationLoader
                 }
                 else
                 {
-                    Guid = PInfo.GUID + ".move";
+                    Guid = "com.illusion";
                     id = animation.id;
                 }
-                return $"{Guid}-{mode}-{id:D3}";
+                return withguid ? $"{Guid}-{mode}-{id:D3}" : $"{mode}-{id:D3}";
             }
 
             public static string GetKey(SwapAnimationInfo animation)
             {
-                return $"{animation.Guid}-{animation.Mode}-{animation.StudioId:D3}";
+                return $"{animation.Guid}-{animation.Mode}-{animation.ControllerFemale}-{animation.StudioId:D3}";
             }
 
             public void SetAnimation(object animation)
             {
                 AnimationInfoHelper((HSceneProc.AnimationListInfo)animation);
+            }
+
+            public static bool IsAnimationLoader(SwapAnimationInfo animation)
+            {
+                return true;
+            }
+
+            public static bool IsAnimationLoader(HSceneProc.AnimationListInfo animation)
+            {
+                swapAnimationMapping.TryGetValue(animation, out var anim);
+                if (anim != null)
+                {
+                    return true;
+                }
+                return false;
             }
         }
     }
