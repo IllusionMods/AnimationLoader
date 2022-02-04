77b799b4b75780ba78d9f613ddf0b40b082e68be
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-04 08:49:43 -0500

    Fix replace List for HashSet

--- src/AnimationLoader.KoikatsuSunshine/UsedAnimations.cs
+++ src/AnimationLoader.KoikatsuSunshine/UsedAnimations.cs
@@ -16,7 +16,7 @@ namespace AnimationLoader
     public class UsedAnimations
     {
         [XmlElement]
-        public List<string> Keys = new();
+        public HashSet<string> Keys = new();
 
         private static readonly string _path = Path.Combine(UserData.Path, "save");
         private static readonly string _fileName = $"{_path}/animations.xml";
@@ -64,7 +64,12 @@ namespace AnimationLoader
                 StreamReader reader = new(_fileName);
                 var tmp = (UsedAnimations)_xmlSerializer.Deserialize(reader.BaseStream);
                 reader.Close();
-                Keys = tmp.Keys;
+                // This can be removed later for some reason was using a List instead of a HashSet
+                // Removing duplicates.
+                foreach (var e in tmp.Keys)
+                {
+                    Keys.Add(e);
+                }
             }
         }
     }
