4ffa53b6222c954b16bf1cdc0f0b077afd0eb83d
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-03 19:31:51 -0500

    Change to pre-release version

--- src/AnimationLoader.Core/Plugin.Info.cs
+++ src/AnimationLoader.Core/Plugin.Info.cs
@@ -14,7 +14,7 @@ using AnimationLoader;
 
 #endregion Assembly attributes
 
-// Log ID: 0020, 0021, 0022, 0023, 0024, 0026, 0027, 0034
+// Log ID: 0022, 0023, 0024, 0026, 0027, 0034
 
 namespace AnimationLoader
 {
@@ -22,7 +22,7 @@ namespace AnimationLoader
     {
         internal const string GUID = "essuhauled.animationloader";
         internal const string PluginDisplayName = "Animation Loader";
-        internal const string Version = "1.1.1.0";
+        internal const string Version = "1.1.0.1";
 #if KK
         internal const string PluginName = "AnimationLoader.Koikatu";
 #elif KKS
