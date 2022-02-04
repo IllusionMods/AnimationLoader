5fccdc6b1b050e75d21e1cebf4a2b1537b010c09
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-03 19:30:05 -0500

    Add test mode and user overrides configuration entries

--- src/AnimationLoader.Core/Plugin.ConfigEntries.cs
+++ src/AnimationLoader.Core/Plugin.ConfigEntries.cs
@@ -1,66 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿//
+// Configuration entries
+//
 using BepInEx.Configuration;
 using BepInEx.Logging;
 
 using KKAPI;
-using KKAPI.Chara;
-using KKAPI.MainGame;
 using KKAPI.Utilities;
 
 using UnityEngine;
-
-using static HFlag;
 
 
 namespace AnimationLoader
 {
     public partial class SwapAnim
     {
-        internal static ConfigEntry<bool> SortPositions { get; set; }
 #if KKS
         internal static ConfigEntry<bool> LoadInCharStudio { get; set; }
+        internal static ConfigEntry<bool> UseAnimationLevels { get; set; }
 #endif
 #if KK
         private static ConfigEntry<bool> UseGrid { get; set; }
 #endif
-        internal static ConfigEntry<KeyboardShortcut> ReloadManifests { get; set; }
+#if DEBUG
+        internal static ConfigEntry<bool> TestMode { get; set; }
+#endif
         internal static ConfigEntry<bool> DebugInfo { get; set; }
+        internal static ConfigEntry<bool> UserOverrides { get; set; }
+        internal static ConfigEntry<KeyboardShortcut> ReloadManifests { get; set; }
         internal static ConfigEntry<bool> Reposition { get; set; }
-        internal static ConfigEntry<bool> UseAnimationLevels { get; set; }
+        internal static ConfigEntry<bool> SortPositions { get; set; }
 
         internal const string GeneralSection = "General";
+        internal const string DebugSection = "Debug";
+        internal const string AdvanceSection = "Advance";
 
         internal void ConfigEntries()
         {
-            ReloadManifests = Config.Bind(
-                section: GeneralSection,
-                key: nameof(ReloadManifests),
-                defaultValue: new KeyboardShortcut(KeyCode.None),
-                configDescription: new ConfigDescription(
-                    description: "Load positions from all manifest format xml files inside " +
-                        "config/AnimationLoader folder",
-                    tags: new ConfigurationManagerAttributes { Order = 10 }));
-
-            SortPositions = Config.Bind(
-            section: GeneralSection,
-            key: nameof(SortPositions),
-            defaultValue: true,
-            configDescription: new ConfigDescription(
-                description: "Sort positions alphabetically",
-                tags: new ConfigurationManagerAttributes { Order = 8 }));
-
 #if KKS
-
             LoadInCharStudio = Config.Bind(
                 section: GeneralSection,
                 key: "Character Studio",
                 defaultValue: true,
                 configDescription: new ConfigDescription(
-                    description: "Disabled module for Studio",
-                    tags: new ConfigurationManagerAttributes { Order = 6 }));
+                    description: "Enable/Disabled module for Studio",
+                    tags: new ConfigurationManagerAttributes { Order = 35 }));
             if (KoikatuAPI.GetCurrentGameMode() == GameMode.Studio)
             {
                 if (!LoadInCharStudio.Value)
@@ -72,16 +55,14 @@ namespace AnimationLoader
             }
 
             UseAnimationLevels = Config.Bind(
-                section: GeneralSection,
-                key: "Use Animation Levels",
+                section: AdvanceSection,
+                key: "Use Experience Levels",
                 defaultValue: true,
                 configDescription: new ConfigDescription(
-                    description: "Apply optional experience levels to animations",
-                    tags: new ConfigurationManagerAttributes { Order = 5, IsAdvanced = true }));
-
+                    description: "Apply experience levels to animations",
+                    tags: new ConfigurationManagerAttributes { Order = 17, IsAdvanced = true }));
 #endif
 #if KK
-
             // TODO: Grid UI for KKS
             // How many animations will require a scrollable grid
             UseGrid = Config.Bind(
@@ -92,29 +73,56 @@ namespace AnimationLoader
                     description: "If you don't want to use the scrollable" +
                         " list for some reason",
                     tags: new ConfigurationManagerAttributes { Order = 6 }));
-
 #endif
+
+            // Load manifests from configuration directory
+            ReloadManifests = Config.Bind(
+                section: GeneralSection,
+                key: nameof(ReloadManifests),
+                defaultValue: new KeyboardShortcut(KeyCode.None),
+                configDescription: new ConfigDescription(
+                    description: "Load positions from all manifest format xml files inside " +
+                        "config/AnimationLoader folder",
+                    tags: new ConfigurationManagerAttributes { Order = 39 }));
+
+            SortPositions = Config.Bind(
+            section: GeneralSection,
+            key: nameof(SortPositions),
+            defaultValue: true,
+            configDescription: new ConfigDescription(
+                description: "Sort positions alphabetically",
+                tags: new ConfigurationManagerAttributes { Order = 37 }));
 
             // Reposition characters in the animations it can help with clipping
             Reposition = Config.Bind(
-                section: GeneralSection,
+                section: AdvanceSection,
                 key: "Reposition Characters",
                 defaultValue: true,
                 configDescription: new ConfigDescription(
                     description: "Reposition characters in the animation before it starts",
                     acceptableValues: null,
-                    tags: new ConfigurationManagerAttributes { Order = 4, IsAdvanced = true }));
+                    tags: new ConfigurationManagerAttributes { Order = 19, IsAdvanced = true }));
+
+            // Save names in UserData.  These can be expanded to other fields if need be.
+            UserOverrides = Config.Bind(
+                section: AdvanceSection,
+                key: "Save Names",
+                defaultValue: false,
+                configDescription: new ConfigDescription(
+                    description: "Save and use names stored in UserData/AnimationLoader/Names. " +
+                        "Names then can be customized and won't be overwritten by updates.",
+                    tags: new ConfigurationManagerAttributes { Order = 17, IsAdvanced = true }));
 
             // To generate debug information this has to be enabled
-            // Almost all Logs are in conditional compilation
+            // The majority of the Logs are in conditional compilation
             DebugInfo = Config.Bind(
-                section: "Debug",
+                section: DebugSection,
                 key: "Debug Information",
                 defaultValue: false,
                 configDescription: new ConfigDescription(
                     description: "Show debug information",
                     acceptableValues: null,
-                    tags: new ConfigurationManagerAttributes { Order = 2, IsAdvanced = true }));
+                    tags: new ConfigurationManagerAttributes { Order = 29, IsAdvanced = true }));
             DebugInfo.SettingChanged += (_sender, _args) =>
             {
                 Log.Enabled = DebugInfo.Value;
@@ -125,7 +133,14 @@ namespace AnimationLoader
             Log.Enabled = DebugInfo.Value;
 #if DEBUG
             Log.Level(LogLevel.Info, $"0028: Log.Enabled set to {Log.Enabled}");
-            Log.Level(LogLevel.Info, $"0028B: Levels set to {UseAnimationLevels.Value}");
+            TestMode = Config.Bind(
+                section: DebugSection,
+                key: "Enable Test Mode",
+                defaultValue: false,
+                configDescription: new ConfigDescription(
+                    description: "Allow unused animations in Free-H",
+                    acceptableValues: null,
+                    tags: new ConfigurationManagerAttributes { Order = 27, IsAdvanced = true }));
 #endif
         }
     }
