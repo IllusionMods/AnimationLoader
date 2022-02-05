f24b0407b83713f1804a6ae1964e65e3b225c7a0
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-05 18:10:13 -0500

    Merge back to regular project

---
+++ src/AnimationLoader.KoikatsuSunshine/AnimationLoaderGameController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using KKAPI.MainGame;
+using UnityEngine;
+
+using static AnimationLoader.SwapAnim;
+
+namespace AnimationLoader
+{
+    internal class AnimationLoaderGameController : GameCustomFunctionController
+    {
+        protected override void OnEndH(MonoBehaviour proc, HFlag flags, bool vr)
+        {
+            if (flags.isFreeH)
+            {
+                return;
+            }
+ 
+            try 
+            { 
+                _usedAnimations.Save();
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"0033: Error saving used animations - {ex}");
+            }
+        }
+    }
+}
