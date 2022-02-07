983bc7f91760300fd561ecd0c101840b374c9a1d
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-06 22:31:33 -0500

    Load optimizations

--- src/AnimationLoader.Core/Hooks.Studio.cs
+++ src/AnimationLoader.Core/Hooks.Studio.cs
@@ -23,67 +23,63 @@ namespace AnimationLoader
             [HarmonyPatch(typeof(Studio.Info), nameof(Studio.Info.LoadExcelDataCoroutine))]
             private static void LoadStudioAnims(Studio.Info __instance, ref IEnumerator __result)
             {
-                Log.Warning("Starting the plugin");
+                var bCached = false;
 
-                __result = __result.AppendCo(() =>
+                _animationClips.Read();
+
+                if (_animationClips.Clips.Keys.Count > 0)
                 {
-                    foreach (var keyVal in animationDict)
+                    //Log.Warning("Patch Cache found");
+                    bCached = true;
+                }
+                else
+                {
+                    Log.Error("Cache not found!");
+                }
+
+                if (bCached)
+                {
+                    __result = __result.AppendCo(() =>
                     {
-                        CreateGroup(0);
-                        CreateGroup(1);
-
-                        void CreateGroup(byte sex)
+                        foreach (var keyVal in animationDict)
                         {
-                            var grp = new Info.GroupInfo { name = $"AL {(sex == 0 ? "M" : "F")} {keyVal.Key}" };
-                            var animGrp =
-                                new Dictionary<int, Dictionary<int, Info.AnimeLoadInfo>>();
+                            CreateGroup(0);
+                            CreateGroup(1);
 
-                            var grpKey = $"{keyVal.Key}{sex}";
-                            if (!EModeGroups.TryGetValue(grpKey, out var grpId))
+                            void CreateGroup(byte sex)
                             {
-                                return;
-                            }
+                                var grp = new Info.GroupInfo { name = $"AL {(sex == 0 ? "M" : "F")} {keyVal.Key}" };
+                                var animGrp = new Dictionary<int, Dictionary<int, Info.AnimeLoadInfo>>();
 
-                            foreach (var swapAnimInfo in keyVal.Value.Where(x => x.StudioId >= 0))
-                            {
-                                var path =
-                                    sex == 0 ? swapAnimInfo.PathMale
-                                    : swapAnimInfo.PathFemale;
-                                var ctrl =
-                                    sex == 0 ? swapAnimInfo.ControllerMale
-                                    : swapAnimInfo.ControllerFemale;
-
-                                if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(ctrl))
+                                var grpKey = $"{keyVal.Key}{sex}";
+                                if (!EModeGroups.TryGetValue(grpKey, out var grpId))
                                 {
-                                    continue;
+                                    return;
                                 }
 
-                                if (_controller is null)
+                                foreach (var swapAnimInfo in keyVal.Value.Where(x => x.StudioId >= 0))
                                 {
-                                    _controller = AssetBundleManager.LoadAsset(
-                                    path,
-                                    ctrl,
-                                    typeof(RuntimeAnimatorController)).GetAsset<RuntimeAnimatorController>();
-                                }
+                                    var path = sex == 0 ? swapAnimInfo.PathMale : swapAnimInfo.PathFemale;
+                                    var ctrl = sex == 0 ? swapAnimInfo.ControllerMale : swapAnimInfo.ControllerFemale;
 
-                                if (_controller == null)
-                                {
-                                    continue;
-                                }
+                                    if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(ctrl))
+                                    {
+                                        continue;
+                                    }
 
-                                var animName = string.IsNullOrEmpty(swapAnimInfo.AnimationName) ?
-                                    ctrl : swapAnimInfo.AnimationName;
-                                grp.dicCategory.Add(swapAnimInfo.StudioId, animName);
-                                var animCat = new Dictionary<int, Info.AnimeLoadInfo>();
-                                animGrp.Add(swapAnimInfo.StudioId, animCat);
+                                    var animName = string.IsNullOrEmpty(swapAnimInfo.AnimationName) ? ctrl : swapAnimInfo.AnimationName;
+                                    grp.dicCategory.Add(swapAnimInfo.StudioId, animName);
+                                    var animCat = new Dictionary<int, Info.AnimeLoadInfo>();
+                                    animGrp.Add(swapAnimInfo.StudioId, animCat);
+                                    List<string> clipsName = new();
 
-                                var clips = _controller.animationClips;
-                                for (var i = 0; i < clips.Length; i++)
-                                {
-                                    var newSlot = UniversalAutoResolver.GetUniqueSlotID();
+                                    var clips = _animationClips.Clips[$"{grpKey}-{ctrl}"];
 
-                                    UniversalAutoResolver.LoadedStudioResolutionInfo
-                                        .Add(new StudioResolveInfo {
+                                    for (var i = 0; i < clips.Count; i++)
+                                    {
+                                        var newSlot = UniversalAutoResolver.GetUniqueSlotID();
+
+                                        UniversalAutoResolver.LoadedStudioResolutionInfo.Add(new StudioResolveInfo {
                                             GUID = swapAnimInfo.Guid,
                                             Slot = i,
                                             ResolveItem = true,
@@ -92,24 +88,113 @@ namespace AnimationLoader
                                             Category = swapAnimInfo.StudioId
                                         });
 
-                                    animCat.Add(newSlot, new Info.AnimeLoadInfo {
-                                        name = clips[i].name,
-                                        bundlePath = path,
-                                        fileName = ctrl,
-                                        clip = clips[i].name,
-                                    });
+                                        animCat.Add(newSlot, new Info.AnimeLoadInfo {
+                                            name = clips[i],
+                                            bundlePath = path,
+                                            fileName = ctrl,
+                                            clip = clips[i],
+                                        });
+
+                                    }
+                                }
+
+                                if (animGrp.Count > 0)
+                                {
+                                    __instance.dicAGroupCategory.Add(grpId, grp);
+                                    __instance.dicAnimeLoadInfo.Add(grpId, animGrp);
                                 }
                             }
+                        }
+                    });
+                }
+                else
+                {
+                    __result = __result.AppendCo(() =>
+                    {
+                        foreach (var keyVal in animationDict)
+                        {
+                            CreateGroup(0);
+                            CreateGroup(1);
 
-                            if (animGrp.Count > 0)
+                            void CreateGroup(byte sex)
                             {
-                                __instance.dicAGroupCategory.Add(grpId, grp);
-                                __instance.dicAnimeLoadInfo.Add(grpId, animGrp);
+                                var grp = new Info.GroupInfo { name = $"AL {(sex == 0 ? "M" : "F")} {keyVal.Key}" };
+                                var animGrp = new Dictionary<int, Dictionary<int, Info.AnimeLoadInfo>>();
+
+                                var grpKey = $"{keyVal.Key}{sex}";
+                                if (!EModeGroups.TryGetValue(grpKey, out var grpId))
+                                {
+                                    return;
+                                }
+
+                                foreach (var swapAnimInfo in keyVal.Value.Where(x => x.StudioId >= 0))
+                                {
+                                    var path = sex == 0 ? swapAnimInfo.PathMale : swapAnimInfo.PathFemale;
+                                    var ctrl = sex == 0 ? swapAnimInfo.ControllerMale : swapAnimInfo.ControllerFemale;
+
+                                    if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(ctrl))
+                                    {
+                                        continue;
+                                    }
+
+                                    var animName = string.IsNullOrEmpty(swapAnimInfo.AnimationName) ? ctrl : swapAnimInfo.AnimationName;
+                                    grp.dicCategory.Add(swapAnimInfo.StudioId, animName);
+                                    var animCat = new Dictionary<int, Info.AnimeLoadInfo>();
+                                    animGrp.Add(swapAnimInfo.StudioId, animCat);
+                                    List<string> clipsName = new();
+
+                                    var controller = AssetBundleManager.LoadAsset(
+                                        path,
+                                        ctrl,
+                                        typeof(RuntimeAnimatorController)).GetAsset<RuntimeAnimatorController>();
+
+                                    var clips = controller.animationClips;
+
+                                    if (controller == null)
+                                    {
+                                        continue;
+                                    }
+
+                                    for (var i = 0; i < clips.Length; i++)
+                                    {
+                                        var newSlot = UniversalAutoResolver.GetUniqueSlotID();
+
+                                        UniversalAutoResolver.LoadedStudioResolutionInfo.Add(new StudioResolveInfo {
+                                            GUID = swapAnimInfo.Guid,
+                                            Slot = i,
+                                            ResolveItem = true,
+                                            LocalSlot = newSlot,
+                                            Group = grpId,
+                                            Category = swapAnimInfo.StudioId
+                                        });
+
+                                        animCat.Add(newSlot, new Info.AnimeLoadInfo {
+                                            name = clips[i].name,
+                                            bundlePath = path,
+                                            fileName = ctrl,
+                                            clip = clips[i].name,
+                                        });
+                                        clipsName.Add(clips[i].name);
+                                    }
+                                    if (!_animationClips.Clips.ContainsKey($"{grpKey}-{ctrl}"))
+                                    {
+                                        _animationClips.Clips.Add($"{grpKey}-{ctrl}", clipsName);
+                                    }
+                                }
+
+                                if (animGrp.Count > 0)
+                                {
+                                    __instance.dicAGroupCategory.Add(grpId, grp);
+                                    __instance.dicAnimeLoadInfo.Add(grpId, animGrp);
+                                }
                             }
                         }
-                    }
-                });
+                        _animationClips.Save();
+                    });
+                }
+
             }
+
 
 
             private static void LoadStudioAnimsOriginal(Studio.Info __instance, ref IEnumerator __result)
