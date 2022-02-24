469ffc14965285ecb07c76e892a3927f5cae0c5c
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-24 11:37:45 -0500

    Meta-data changes

--- src/AnimationLoader.KoikatsuSunshine/Plugin.cs
+++ src/AnimationLoader.KoikatsuSunshine/Plugin.cs
@@ -2,14 +2,15 @@
 // Entry point for AnimationLoader.KoikatsuSunshine
 //
 using BepInEx;
+
 using KKAPI;
 
 
 namespace AnimationLoader
 {
-    [BepInPlugin(PInfo.GUID, PInfo.PluginDisplayName, PInfo.Version)]
     [BepInDependency(Sideloader.Sideloader.GUID, Sideloader.Sideloader.Version)]
     [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    [BepInPlugin(GUID, PluginDisplayName, Version)]
     [BepInProcess(KoikatuAPI.GameProcessName)]
     [BepInProcess(KoikatuAPI.StudioProcessName)]
     [BepInProcess(KoikatuAPI.VRProcessName)]
