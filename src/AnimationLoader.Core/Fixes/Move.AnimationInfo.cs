6ab7c0095970c954900db39474b258bc37611d6c
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-04-19 21:23:31 -0400

    Move static methods out of AnimationInfo class

--- src/AnimationLoader.Core/Fixes/Move.AnimationInfo.cs
+++ src/AnimationLoader.Core/Fixes/Move.AnimationInfo.cs
@@ -31,6 +31,7 @@ namespace AnimationLoader
             public string Key => $"{_Guid}-{_mode}-{_controller}-{_id:D3}";
             public EMode Mode => _mode;
             public string Name => _name;
+            public bool IsAnimationLoader => _isAnimationLoader;
             public SwapAnimationInfo SwapAnim => _anim;
             public HSceneProc.AnimationListInfo Animation => _animGA;
 
@@ -89,12 +90,6 @@ namespace AnimationLoader
                 AnimationInfoHelper((HSceneProc.AnimationListInfo)animation);
             }
 
-            // IsAnimationLoader no parameters refers to instance
-            public bool IsAnimationLoader()
-            {
-                return _isAnimationLoader;
-            }
-
             public string TranslateName()
             {
                 return Utilities.TranslateName(_name);
@@ -108,76 +103,52 @@ namespace AnimationLoader
                 }
                 return _anim;
             }
-            /// <summary>
-            /// Static function to get the key for any animation 
-            /// no need for and instance of the class
-            /// </summary>
-            /// <param name="animation"></param>
-            /// <param name="withguid"></param>
-            /// <returns></returns>
-            public static string GetKey(
-                HSceneProc.AnimationListInfo animation,
-                bool withguid = true)
-            {
-                string Guid;
-                EMode mode;
-                int id;
-                string controller;
 
-                mode = animation.mode;
-                swapAnimationMapping.TryGetValue(animation, out var anim);
-                if (anim != null)
-                {
-                    Guid = anim.Guid;
-                    id = anim.StudioId;
-                    controller = anim.ControllerFemale;
-                }
-                else
-                {
-                    Guid = "com.illusion";
-                    id = animation.id;
-                    controller = animation.paramFemale.path.file;
-                }
-                return withguid ? 
-                    $"{Guid}-{mode}-{controller}-{id:D3}" : $"{mode}-{controller}-{id:D3}";
-            }
+        }
 
-            /// <summary>
-            /// GetKey overload
-            /// </summary>
-            /// <param name="animation"></param>
-            /// <returns></returns>
-            public static string GetKey(SwapAnimationInfo animation)
-            {
-                return $"{animation.Guid}-{animation.Mode}-{animation.ControllerFemale}" +
-                    $"-{animation.StudioId:D3}";
-            }
+        /// <summary>
+        /// Static function to get the key for any animation 
+        /// no need for and instance of the class
+        /// </summary>
+        /// <param name="animation"></param>
+        /// <param name="withguid"></param>
+        /// <returns></returns>
+        public static string GetAnimationKey(
+            HSceneProc.AnimationListInfo animation,
+            bool withguid = true)
+        {
+            string Guid;
+            EMode mode;
+            int id;
+            string controller;
 
-            // IsAnimationLoader static check if it parameter is an AnimationLoader animation
-            public static bool IsAnimationLoader(SwapAnimationInfo animation)
+            mode = animation.mode;
+            swapAnimationMapping.TryGetValue(animation, out var anim);
+            if(anim != null)
             {
-                return true;
+                Guid = anim.Guid;
+                id = anim.StudioId;
+                controller = anim.ControllerFemale;
             }
+            else
+            {
+                Guid = "com.illusion";
+                id = animation.id;
+                controller = animation.paramFemale.path.file;
+            }
+            return withguid ?
+                $"{Guid}-{mode}-{controller}-{id:D3}" : $"{mode}-{controller}-{id:D3}";
+        }
 
-            public static bool IsAnimationLoader(HSceneProc.AnimationListInfo animation)
-            {
-                swapAnimationMapping.TryGetValue(animation, out var anim);
-                if (anim != null)
-                {
-                    return true;
-                }
-                return false;
-            }
-
-            public static string TranslateName(HSceneProc.AnimationListInfo animation)
-            {
-                swapAnimationMapping.TryGetValue(animation, out var anim);
-                if (anim != null)
-                {
-                    return Utilities.TranslateName(anim.AnimationName);
-                }
-                return Utilities.TranslateName(animation.nameAnimation);
-            }
+        /// <summary>
+        /// GetKey overload
+        /// </summary>
+        /// <param name="animation"></param>
+        /// <returns></returns>
+        public static string GetAnimationKey(SwapAnimationInfo animation)
+        {
+            return $"{animation.Guid}-{animation.Mode}-{animation.ControllerFemale}" +
+                $"-{animation.StudioId:D3}";
         }
     }
 }
