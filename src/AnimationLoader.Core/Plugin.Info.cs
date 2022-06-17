4355c4b25acffec175899562d38752a0260f54b1
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-06-17 05:35:03 -0400

    Bump version

--- src/AnimationLoader.Core/Plugin.Info.cs
+++ src/AnimationLoader.Core/Plugin.Info.cs
@@ -27,7 +27,7 @@ namespace AnimationLoader
 #else
         public const string PluginDisplayName = "Animation Loader";
 #endif
-        public const string Version = "1.1.2.1";
+        public const string Version = "1.1.2.2";
 #if KK
         public const string PluginName = "AnimationLoader.Koikatu";
 #else
