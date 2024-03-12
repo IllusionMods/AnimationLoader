c72d3e82b0e64ab1ee552d0495470856ded92c3e
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2024-03-12 03:09:47 -0400

    Update file

--- src/AnimationLoader.Koikatu/GlobalSuppressions.cs
+++ src/AnimationLoader.Koikatu/GlobalSuppressions.cs
@@ -6,7 +6,7 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "Style use by community", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.GetMoveController(ChaControl)~AnimationLoader.SwapAnim.MoveController")]
-[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "Style use by community", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.MoveController.SetOriginalPosition")]
+//[assembly: SuppressMessage("Style", "IDE0022:Use block body for methods", Justification = "Style use by community", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.MoveController.SetOriginalPosition")]
 [assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.ReadNames(AnimationLoader.SwapAnim.Names@,System.String)")]
 [assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:AnimationLoader.SwapAnim.SaveNamesXmls")]
 [assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "Variable modified in code", Scope = "member", Target = "~F:AnimationLoader.SwapAnim.Hooks._animationClipsCache")]
