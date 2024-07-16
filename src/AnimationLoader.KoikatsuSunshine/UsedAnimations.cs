6862bf485fb4c0754b214c709909198ec5dfe5de
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2024-07-16 00:54:48 -0400

    Make backup of last known good file

--- src/AnimationLoader.KoikatsuSunshine/UsedAnimations.cs
+++ src/AnimationLoader.KoikatsuSunshine/UsedAnimations.cs
@@ -22,8 +22,10 @@ namespace AnimationLoader
 
         private static readonly string _path = Path.Combine(UserData.Path, "save");
         private static readonly string _fileName = $"{_path}/animations.xml";
+        private static readonly string _bkFileName = $"{_path}/animations.bk";
         private static readonly XmlSerializer _xmlSerializer = new(typeof(UsedAnimations));
         private static readonly FileInfo _fileInfo = new(_fileName);
+        private static readonly FileInfo _bkFileInfo = new(_fileName);
 
         public override string ToString()
         {
@@ -69,11 +71,46 @@ namespace AnimationLoader
                     {
                         Keys.Add(e);
                     }
+
+                    // Make backup at this point the xml.Deserialize should have worked 
+                    if (!_bkFileInfo.Exists)
+                    {
+                        _fileInfo.CopyTo(_bkFileName);
+                    }
+                    else
+                    {
+                        // This assumes no manual editing (maybe crc32 someday)
+                        if (_bkFileInfo.Length != _fileInfo.Length)
+                        {
+                            _fileInfo.CopyTo(_bkFileName, true);
+                        }
+                    }
                 }
                 catch
                 {
                     Log.Level(LogLevel.Error, $"[UsedAnimations.Read] File: {_fileName} corrupt " +
                         "the file will be overwritten on game exit.");
+
+                    if (_bkFileInfo.Exists)
+                    {
+                        try
+                        {
+                            Log.Level(LogLevel.Error, $"[UsedAnimations.Read] Trying backup.");
+                            StreamReader reader = new(_bkFileName);
+                            var tmp = (UsedAnimations)_xmlSerializer.Deserialize(reader.BaseStream);
+                            reader.Close();
+                            // This can be removed later for some reason was using a List instead of
+                            // a HashSet Removing duplicates.
+                            foreach (var e in tmp.Keys)
+                            {
+                                Keys.Add(e);
+                            }
+                        }
+                        catch
+                        {
+                            Log.Level(LogLevel.Error, $"[UsedAnimations.Read] Can't read backup.");
+                        }
+                    }
                 }
             }
         }
